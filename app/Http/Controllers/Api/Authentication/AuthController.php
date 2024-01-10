892aa006b06bd8c37802d5a7fe4b552f5c3986b2
INRDEVTHIYAGLT1\sthiyagarajan <sthiyagarajan@uplandsoftware.com>
2024-01-10 19:04:20 +0530

    Refactored for more code readability

--- app/Http/Controllers/Api/Authentication/AuthController.php
+++ app/Http/Controllers/Api/Authentication/AuthController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\Api\Authentication;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
+    public function login(Request $request): JsonResponse
     {
         $request->validate([
             'email' => ['required', 'email'],
@@ -17,17 +18,19 @@ class AuthController extends Controller
             'device_name' => ['required'],
         ]);
 
-        $user = User::where('email', $request->email)->first();
+        $user = User::query()
+            ->where('email', $request->get('email'))
+            ->first();
 
         if (!$user) {
             return response()->json(["message" => 'User does not exist'], 422);
         }
 
-        if (!Hash::check($request->password, $user->password)) {
+        if (!Hash::check($request->get('password'), $user->password)) {
             return response()->json(["message" => "Password mismatch"], 422);
         }
 
-        $token = $user->createToken($request->device_name . "-" . (count($user->tokens) + 1))->plainTextToken;
+        $token = $user->createToken($request->get('device_name') . "-" . (count($user->tokens) + 1))->plainTextToken;
 
         $response = [
             'token' => $token,
@@ -37,19 +40,19 @@ class AuthController extends Controller
         return response()->json($response, 200);
     }
 
-    public function user()
+    public function user(): JsonResponse
     {
         return request()->user();
     }
 
-    public function logout()
+    public function logout(): JsonResponse
     {
         // Revoke the token that was used to authenticate the current request
         request()->user()->currentAccessToken()->delete();
         return response()->json([], 204);
     }
 
-    public function logoutFromAllDevices()
+    public function logoutFromAllDevices(): JsonResponse
     {
         // Revoke all tokens
         request()->user()->tokens()->delete();
